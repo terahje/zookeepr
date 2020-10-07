c94dd007247a40a83119f9cf1733dd3187c3982a
terahje <terahjequinn@gmail.com>
2020-10-07 13:10:43 -0700

    updated requests or sepcific animal search

--- server.js
+++ server.js
@@ -44,14 +44,29 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+      results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
+
