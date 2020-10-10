c6718418da79254814aae8326e6227ea0d3ecc44
terahje <terahjequinn@gmail.com>
2020-10-09 21:21:22 -0700

    implemented front end with backend

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
 
 const { animals } = require('./data/animals');
 
@@ -111,7 +113,20 @@ app.get('/api/animals', (req, res) => {
       res.json(animal); 
     }
   });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+   app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  }); 
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
   
+
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
